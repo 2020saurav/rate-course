f98bef029d459cd8bd433a9c928a94a0b5f6135c
Saurav Kumar <2020saurav@gmail.com>
2014-10-10 16:10:45 +0530

    Intial Commit: Hello World Server

---
+++ app.js
@@ -0,0 +1,21 @@
+//dependencies
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+//environments
+app.set('port', process.env.PORT||3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//routes
+app.get('/', routes.index);
+
+
+// server creation
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
