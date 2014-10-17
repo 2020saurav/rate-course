aecaec50e060c19cb1a4a3ca01fcdae1296c8073
Saurav Kumar <2020saurav@gmail.com>
2014-10-17 09:01:49 +0530

    All model structure complete. TODO: complete definitions.

--- models/rating_value.js
+++ models/rating_value.js
@@ -7,3 +7,20 @@
  * value                INTEGER
  * is_deleted           INTEGER(1)
  */
+
+module.exports = function(sequelize, DataTypes) {
+    var RatingValue =  sequelize.define ('rating_value',
+        {
+            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true}
+            // TODO complete this
+
+        },
+        {
+            freezeTableName : true,
+            underscored : true,
+            tableName : 'rating_value',
+            timestamps : false
+        });
+
+    return RatingValue;
+}
