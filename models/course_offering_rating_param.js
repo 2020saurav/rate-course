e9d009480931e01a4768c4c08d322dee9a7ad172
Abhilash Kumar <abhi.iitk16@gmail.com>
2014-10-20 01:08:57 +0530

    commit using webstorm

--- models/course_offering_rating_param.js
+++ models/course_offering_rating_param.js
@@ -13,7 +13,9 @@ module.exports = function(sequelize, DataTypes) {
     var CourseOfferingRatingParam =  sequelize.define ('course_offering_rating_param',
         {
             id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
-            course_offering_id: {type: DataTypes.INTEGER,allowNull: false}
+            course_offering_id: {type: DataTypes.INTEGER,allowNull: false},
+            rating_param_id: {type: DataTypes.INTEGER, allowNull: false}
+
             // TODO complete this
 
         },
