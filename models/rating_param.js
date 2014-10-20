fa6a25be3c658b1a41f38e5bf2cdf3dc62d418ef
Abhilash Kumar <abhi.iitk16@gmail.com>
2014-10-20 16:30:20 +0530

    error resolved

--- models/rating_param.js
+++ models/rating_param.js
@@ -10,10 +10,10 @@
 module.exports = function(sequelize, DataTypes) {
     var RatingParam =  sequelize.define ('rating_param',
         {
-            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true}
+            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
             name: {type: DataTypes.TEXT, allowNull: false},
-	    type: {type: DataTypes.INTEGER, allowNull: false},
-	    sort_order: {type: DataTypes.INTEGER, allowNull: true}
+	       type: {type: DataTypes.INTEGER, allowNull: false},
+	           sort_order: {type: DataTypes.INTEGER, allowNull: true}
 	   // TODO complete this
 
         },
