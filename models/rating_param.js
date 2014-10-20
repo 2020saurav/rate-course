c39478df9f3b198cce0222b1df3daeb6feaf5bd7
Ankita Pasricha <ankitap@cse.iitk.ac.in>
2014-10-20 16:13:03 +0530

    Models by Ankita

--- models/rating_param.js
+++ models/rating_param.js
@@ -11,7 +11,10 @@ module.exports = function(sequelize, DataTypes) {
     var RatingParam =  sequelize.define ('rating_param',
         {
             id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true}
-            // TODO complete this
+            name: {type: DataTypes.TEXT, allowNull: false},
+	    type: {type: DataTypes.INTEGER, allowNull: false},
+	    sort_order: {type: DataTypes.INTEGER, allowNull: true}
+	   // TODO complete this
 
         },
         {
--- models/user.js
+++ models/user.js
@@ -17,8 +17,18 @@
 module.exports = function(sequelize, DataTypes) {
     var User =  sequelize.define ('user',
         {
-            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true}
-            // TODO complete this
+            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
+           login: {type: DataTypes.STRING(40), allowNull: false},}
+	   password: {type: DataTypes.STRING(40), allowNull: false},
+           email: {type: DataTypes.STRING(40), allowNull: false},
+	   first_name: {type: DataTypes.STRING(50), allowNull: false},
+	   last_name: {type: DataTypes.STRING(50)},
+	   last_login: {type: DataTypes.INTEGER},
+	   last_ip: {type: DataTypes.STRING(25)},
+	   is_active: {type: DataTypes.BOOLEAN, defaultValue: false},
+	   photo_url: {type: DataTypes.STRING},
+	   password_token: {type: DataTypes.STRING}
+	    // TODO complete this
 
         },
         {
