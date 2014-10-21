97d6410b75c706ba8a6f956131b38f839da33483
Saurav Kumar <2020saurav@gmail.com>
2014-10-21 10:35:29 +0530

    Course, Course Offering linked with join and foreign key relations

--- app.js
+++ app.js
@@ -18,11 +18,17 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
-app.get('/', routes.index);
-app.get('/course/', routes.course);
+app.get('/', routes.index); // app home
+
+app.get('/course/', routes.courses); // list all courses
+
 app.get('/course/:id/',function(req,res){
+    routes.course(req,res)
+}) // details of selected course
+
+app.get('/course/:id/:offeringId/',function(req,res){
     routes.courseOffering(req,res)
-})
+})// details of selected course offering
 
 // server creation
 http.createServer(app).listen(app.get('port'), function() {
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,70 @@ exports.index = function(req, res) {
 	res.render('index');
 }
 
-exports.course = function(req, res) {
+exports.courses = function(req, res) {
     var courseModel = model.sequelize.models.course;
     courseModel.findAll().success(function(courses) {
-        res.render('course',{
+        res.render('courses',{
             "courses" : courses
         });
     })
-
 }
 
-exports.courseOffering = function(req, res) {
-    res.render('courseOffering',{
-        "courseId" : req.param("id"),
-        "instructor" : "Shubhv"
+exports.course = function(req,res) {
+    var courseModel = model.sequelize.models.course;
+    var courseOfferingModel = model.sequelize.models.course_offering;
+    var profModel = model.sequelize.models.professor;
 
-    });
+    courseModel.hasMany(courseOfferingModel,{foreignKey:'course_id'})
+    courseOfferingModel.belongsTo(courseModel,{foreignKey:'course_id'})
+    profModel.hasMany(courseOfferingModel,{foreignKey:'professor_id'})
+    courseOfferingModel.belongsTo(profModel,{foreignKey:'professor_id'})
+    var courseId = req.param("id");
+
+    courseModel.find({
+        where: {id: courseId},
+        include: [
+                    {
+                        model:courseOfferingModel,
+                        include:[profModel]
+                    }
+        ]
+    }).success(function(course){
+            res.render('course', {
+            "course" : course
+        })
+    })
+}
+exports.courseOffering = function(req, res) {
+
+    var courseModel = model.sequelize.models.course;
+    var courseOfferingModel = model.sequelize.models.course_offering;
+    var profModel = model.sequelize.models.professor;
+
+    courseModel.hasMany(courseOfferingModel,{foreignKey:'course_id'})
+    courseOfferingModel.belongsTo(courseModel,{foreignKey:'course_id'})
+    profModel.hasMany(courseOfferingModel,{foreignKey:'professor_id'})
+    courseOfferingModel.belongsTo(profModel,{foreignKey:'professor_id'})
+
+    var courseId = req.param("offeringId");
+    courseOfferingModel.find({
+        where: {id: courseId},
+        include: [
+            {
+                model:courseModel
+            },
+            {
+                model:profModel
+            }
+        ]
+    }).success(function(courseOffering){
+        if(courseOffering.course.id==req.param("id")) {
+            res.render('courseOffering',{
+            "courseOffering" : courseOffering
+        });
+
+        }
+        else
+            res.send("This offering does not belong this course")
+    })
 }
