16649a6b2b16fce9b7cca41ab1971a87d3b84aa7
Saurav Kumar <2020saurav@gmail.com>
2014-10-27 15:22:39 +0530

    New model for cumulative rating values

---
+++ models/cumulative_rating_value.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param sequelize
+ * @param DataTypes
+ * @returns {*|Model}
+ */
+module.exports = function(sequelize, DataTypes) {
+    var CumulativeRatingValue =  sequelize.define ('cumulative_rating_value',
+        {
+            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
+            course_offering_id: {type: DataTypes.INTEGER, allowNull: false },
+            rating_param_id: {type: DataTypes.INTEGER, allowNull: false},
+            value: {type: DataTypes. INTEGER, allowNull:true}
+        },
+        {
+            freezeTableName : true,
+            underscored : true,
+            tableName : 'cumulative_rating_value',
+            timestamps : false
+        });
+
+    return CumulativeRatingValue;
+}
