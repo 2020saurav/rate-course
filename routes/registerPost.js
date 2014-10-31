aa4da7c8e942913f3535b603e19632c10596a49e
Saurav Kumar <2020saurav@gmail.com>
2014-10-31 17:57:05 +0530

    CC FTP based authentication

--- routes/registerPost.js
+++ routes/registerPost.js
@@ -4,6 +4,26 @@
 3. Set random auth token, set password to be some random string, isactive false
 4. send mail
  */
+var JSFtp = require('jsftp');
+
 module.exports = function (req, res) {
-    res.send(req.body);
-}
+    var cclogin = req.body.login;
+    var ccpassword = req.body.password;
+
+    var Ftp = new JSFtp({
+        host: "webhome.cc.iitk.ac.in",
+        port: 21
+    });
+    Ftp.auth(cclogin,ccpassword,function(err) {
+        if(err)
+        {
+            res.send("False"); // wrong CC password or login
+        }
+        else
+        {
+            res.send("True"); // right login!
+        }
+
+    })
+
+};
