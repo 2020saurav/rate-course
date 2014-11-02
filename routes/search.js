88527206be7f53ef57e15f8fd29e219e428aee7a
Saurav Kumar <2020saurav@gmail.com>
2014-11-02 11:44:35 +0530

    course/professor search

--- routes/search.js
+++ routes/search.js
@@ -9,25 +9,46 @@ var URL = require('url');
 var model = require('../models/index');
 var professorModel = model.sequelize.models.professor;
 var courseModel = model.sequelize.models.course;
+var Sequelize = require('sequelize');
 
 module.exports = function (req, res) {
     var urlParse = URL.parse(req.url, true);
     var query = urlParse.query.q;
-    var responses = [
+    var title, description, url, response;
+    var responses = [];
+    courseModel.findAll({
+        where : Sequelize.or(
+            {"department": query},
+            ["course_name LIKE '%"+query+"%'"],
+            ["course_number LIKE '%"+query+"%'"]
+        )
+    }).success(function(courses){
+        for(var i=0; i<courses.length; i++)
         {
-            "title" : "Title1",
-            "description" : "Desc 1",
-            "url" : "/course/1/"
-        },
-        {
-            "title" : "Title2",
-            "description" : "Desc 2",
-            "url" : "/course/2/"
+            title = courses[i].course_number+" "+courses[i].course_name;
+            description = courses[i].description;
+            url = '/course/'+courses[i].id+'/';
+
+            response = {"title" : title, "description": description, "url" : url};
+            responses.push(response);
         }
-    ];
-    // write sequelize here : put things in responses as shown above TODO
-    // send sorted results from here if need be
-    res.render('searchResult',{"query":query, "responses": responses, "session" : req.session});
+        professorModel.findAll({
+            where : Sequelize.or(
+                ["first_name LIKE '%"+query+"%'"],
+                ["last_name LIKE '%"+query+"%'"],
+                ["email LIKE '%"+query+"%'"]
+            )
+        }).success(function(professors) {
+            for(var i=0; i<professors.length; i++)
+            {
+                title = professors[i].first_name+" "+ professors[i].last_name;
+                description = "Department: "+ professors[i].department;
+                url = '/professor/'+professors[i].id+'/';
 
-
+                response = {"title" : title, "description": description, "url" : url};
+                responses.push(response);
+            }
+            res.render('searchResult',{"query":query, "responses": responses, "session" : req.session});
+        });
+    });
 };
