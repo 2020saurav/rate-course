c7c995364523ff57530aa8a7eca54cd4a7d43496
Shubhangee Verma <shubhv@cse.iitk.ac.in>
2014-11-02 20:49:36 +0530

    discussion listing added on course page

--- routes/course.js
+++ routes/course.js
@@ -1,12 +1,21 @@
 var model = require('../models/index');
+var moment = require('moment');
+
 var courseModel = model.sequelize.models.course;
 var courseOfferingModel = model.sequelize.models.course_offering;
 var profModel = model.sequelize.models.professor;
+var userModel = model.sequelize.models.user;
+var discussionModel = model.sequelize.models.discussion;
 
 courseModel.hasMany(courseOfferingModel,{foreignKey:'course_id'});
 courseOfferingModel.belongsTo(courseModel,{foreignKey:'course_id'});
 profModel.hasMany(courseOfferingModel,{foreignKey:'professor_id'});
 courseOfferingModel.belongsTo(profModel,{foreignKey:'professor_id'});
+
+courseModel.hasMany(discussionModel,{foreignKey:'course_id'});
+discussionModel.belongsTo(courseModel,{foreignKey:'course_id'});
+userModel.hasMany(discussionModel,{foreignKey:'user_id'});
+discussionModel.belongsTo(userModel,{foreignKey:'user_id'});
 
 module.exports = function(req,res) {
     var courseId = req.param("id");
@@ -15,13 +24,27 @@ module.exports = function(req,res) {
         include: [
             {
                 model:courseOfferingModel,
-                include:[profModel]
+                include:[
+                    {
+                        model:profModel
+                    }
+                ]
+            },
+            {
+                model:discussionModel,
+                include:[
+                    {
+                        model: userModel
+                    }
+                ]
             }
         ]
     }).success(function(course){
+//        res.send(course);
         res.render('course', {
             "course" : course,
-            "session":req.session
+            "session":req.session,
+            "moment":moment
         })
     })
 };
