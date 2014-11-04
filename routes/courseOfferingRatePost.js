fa55527a64e53a9312e1e4b3cda038f36144ed15
Saurav Kumar <2020saurav@gmail.com>
2014-11-04 12:36:06 +0530

    Rating Form css changed. As Anon Posting Review

--- routes/courseOfferingRatePost.js
+++ routes/courseOfferingRatePost.js
@@ -8,49 +8,60 @@ var helper = require('./helper');
 var ratingId = 0;
 module.exports = function (req, res) {
     var returnURL;
-    ratingModel.create({
-        user_id: req.session.userId,
-        course_offering_id: req.param("offeringId"),
-        create_time: moment().unix()
-    }).success(function(data){
-        ratingId = data.id;
-        if(ratingId !== 0)
-        {
-//            var courseComment = req.body.courseComment,
-//                professorComment = req.body.professorComment;
-//
-//            if((courseComment && courseComment!==null && courseComment !== "")
-//                || (professorComment && professorComment !== null && professorComment !== ""))
-//            {
-            reviewModel.create({
-                rating_id: ratingId,
-                course_comment: req.body.courseComment,
-                prof_comment: req.body.professorComment
-            });
+    returnURL="/course/"+req.param("id")+"/"+req.param("offeringId") + "/";
 
-//            }
-            var object = req.body;
-            for(var x in  object )
-            {
-                if(x !== 'courseComment' && x !== 'professorComment')
-                {
-                    ratingValueModel.create({
-                        rating_id: ratingId,
-                        rating_param_id: x,
-                        value: object[x]
-                    });
-                }
-            }
-            helper.reCalculateCourseOfferingRating(res,req.param("offeringId"));
-            helper.reCalculateCourseRating(req.param("id"));
-            returnURL="/course/"+req.param("id")+"/"+req.param("offeringId") + "/";
+    ratingModel.findOne({
+        where : {
+            user_id: req.session.userId,
+            course_offering_id: req.param("offeringId")
+        }
+    }).success(function(rating) {
+        if(rating)
+        {
+            // already rated this offering by this user
             res.redirect(returnURL);
         }
         else
         {
-            returnURL="/course/"+req.param("id")+"/"+req.param("offeringId") + "/";
-            res.redirect(returnURL);
+            var as_anon = false;
+            if(req.body.asAnon)
+                as_anon = true;
+            ratingModel.create({
+                user_id: req.session.userId,
+                course_offering_id: req.param("offeringId"),
+                create_time: moment().unix(),
+                as_anon : as_anon
+            }).success(function(data){
+                ratingId = data.id;
+                if(ratingId !== 0)
+                {
+                    reviewModel.create({
+                        rating_id: ratingId,
+                        course_comment: req.body.courseComment,
+                        prof_comment: req.body.professorComment
+                    });
+
+                    var object = req.body;
+                    for(var x in  object )
+                    {
+                        if(x !== 'courseComment' && x !== 'professorComment')
+                        {
+                            ratingValueModel.create({
+                                rating_id: ratingId,
+                                rating_param_id: x,
+                                value: object[x]
+                            });
+                        }
+                    }
+                    helper.reCalculateCourseOfferingRating(res,req.param("offeringId"));
+                    helper.reCalculateCourseRating(req.param("id"));
+                    res.redirect(returnURL);
+                }
+                else
+                {
+                    res.redirect(returnURL);
+                }
+            });
         }
     });
 };
-// TODO here also ensure that rating is not already present!
